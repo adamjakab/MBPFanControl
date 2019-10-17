dfc01cedf55911f29ae722507d69ffe70a6bb60c
jackisback <adaja@itu.dk>
2019-10-17 14:53:35 +0200

    launcher fix

--- service/launcher.sh
+++ service/launcher.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 _DIR_="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." >/dev/null 2>&1 && pwd )"
-# cd "${_DIR_}" || exit 1
-pipenv run "${_DIR_}"/app.py
+cd "${_DIR_}" || exit 1
+/usr/local/bin/pipenv run "${_DIR_}/app.py"
